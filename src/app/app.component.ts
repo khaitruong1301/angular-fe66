cd7e5b290905922665cc3df153043ca7e80140c1
macbook <khaitruong1301@gmail.com>
2021-08-06 21:42:06 +0700

    buoi1_angular_createComponent_databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', //tên thẻ component
+  templateUrl: './app.component.html', //đường dẫn đến giao diện component
+  styleUrls: ['./app.component.scss'] //đường đẫn đến css của component
 })
 export class AppComponent {
   title = 'angularFrontend66';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BaiTapLayoutModule } from './bai-tap-layout/bai-tap-layout.module';
+import { DataBindingModule } from './DataBinding/DataBinding.module';
+import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, //Các component trong module này (Mỗi component sinh ra phải ở trong 1 module)
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BaiTapLayoutModule,DataBindingModule //nơi chèn các module khác vào
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //nơi chèn các service vào
+  bootstrap: [AppComponent] //Các component có thể sử dụng được trong file index.html
 })
-export class AppModule { }
+export class AppModule { } //Module gốc ứng dụng 
